7cc95e589e5b33cd19f8d82825ebc4b2e692dba2
Zach <zachary.krensky@kbrwyle.com>
2018-02-23 09:58:55 -0500

    truffle config boilerplate

--- truffle.js
+++ truffle.js
@@ -1,4 +1,27 @@
+// Truffle configuration file
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    dev: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // match any network
+      // from: `wallet address`
+    },
+
+    prod: {
+      host: "1.1.1.1",
+      port: 8080,
+      network_id: 1,	// Ethereum live network
+      // additional variables
+      // gas
+      // gasPrice
+      // from (contract address)
+      // provider (web3 provider)
+      //
+      //
+  }
 };
+
